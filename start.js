23f89f5c586afc10b116607360496685d48167c0
Shreyansh Singh <91277635+SinghShreyansh@users.noreply.github.com>
2022-12-14 19:48:58 +0530

    Update start.js

--- start.js
+++ start.js
@@ -10,7 +10,7 @@
  });
  
  // Connect to our Database and handle any bad connections
- mongoose.connect(process.env.DATABASE, {
+ mongoose.connect("mongodb+srv://shreyansh:WKv8gJans6YHosoK@cluster0.d9dm4uo.mongodb.net/node?retryWrites=true&w=majority", {
    useCreateIndex: true,
    useNewUrlParser: true,
    useFindAndModify: false,
